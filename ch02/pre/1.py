afa494f44e0ef1fc531856d6c2ec5e0984d93355
leeteukki <tklohj@naver.com>
2019-08-30 16:22:25 +0900

    first commit

---
+++ ch02/pre/1.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+#%%
+a = 1
+b = 2
+print(a)
---
+++ ch02/pre/untitled2.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Aug 30 12:56:26 2019
+
+@author: leechat
+"""
+
+a = 1
+b = 3
+bread = '크림빵'
+
+print(b)
